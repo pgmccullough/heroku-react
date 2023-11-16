6d8271d737db3530e5f8bf3b3daf1f5fd2a7f404
Patrick Glendon McCullough <p@mccullo.ug>
2023-11-16 12:27:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You already know React
         </a>
       </header>
     </div>
